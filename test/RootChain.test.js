6c22b9461b18d7b3a1ee61d509ee4583a0db6867
4000D <dkdkajej@gmail.com>
2018-10-17 14:39:24 +0900

    test: refactor test cases

--- test/RootChain.test.js
+++ test/RootChain.test.js
@@ -32,9 +32,11 @@ contract('RootChain', async ([
   let MAX_REQUESTS;
   let NRBEpochLength; // == 2
   let COST_ERO, COST_ERU, COST_URB_PREPARE, COST_URB, COST_ORB, COST_NRB;
+
   const currentFork = 0;
   let currentEpoch = 1;
   let currentBlockNumber = 0;
+  let numRequests = 0;
 
   // rootchain.State
   const State = {
@@ -93,8 +95,10 @@ contract('RootChain', async ([
     await checkBlockNumber();
   }
 
-  describe('Epoch#1', async () => {
-    before('check NRB epoch#1 parameters', async () => {
+  const testEpochsWithoutRequest = (NRBEPochNumber) => {
+    const ORBEPochNumber = NRBEPochNumber + 1;
+
+    before(`check NRB epoch#${NRBEPochNumber} parameters`, async () => {
       await checkState(State.AcceptingNRB);
       await checkBlockNumber();
 
@@ -105,7 +109,7 @@ contract('RootChain', async ([
       endBlockNumber.should.be.bignumber.equal(startBlockNumber.add(NRBEpochLength).sub(1));
     });
 
-    it('next empty ORB epoch#2 should be prepared', async () => {
+    it(`next empty ORB epoch#${ORBEPochNumber} should be prepared`, async () => {
       // submits `NRBEpochLength` NRBs
       await Promise.all(range(NRBEpochLength).map(submitDummyNRB));
 
@@ -116,7 +120,7 @@ contract('RootChain', async ([
       await checkState(State.AcceptingNRB);
     });
 
-    after('check ORB epoch#2 parameters', async () => {
+    after(`check ORB epoch#${ORBEPochNumber} parameters`, async () => {
       const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
         await rootchain.getEpoch(currentFork, currentEpoch - 1);
 
@@ -124,93 +128,31 @@ contract('RootChain', async ([
       endBlockNumber.should.be.bignumber.equal(endBlockNumber);
       isEmpty.should.be.equal(true);
     });
-  });
+  };
 
-  describe('Epoch#3', async () => {
-    before('check parameters', async () => {
-      await checkState(State.AcceptingNRB);
-      await checkBlockNumber();
+  const testEpochsWithRequest = (NRBEPochNumber) => {
+    const ORBEPochNumber = NRBEPochNumber + 1;
 
-      const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
-        await rootchain.getEpoch(currentFork, currentEpoch);
-
-      startBlockNumber.should.be.bignumber.equal(currentBlockNumber + 1);
-      endBlockNumber.should.be.bignumber.equal(startBlockNumber.add(NRBEpochLength).sub(1));
-    });
-
-    it('next empty ORB epoch#4 should be prepared', async () => {
-      // submits `NRBEpochLength` NRBs
-      await Promise.all(range(NRBEpochLength).map(submitDummyNRB));
-
-      currentEpoch += 2;
-
-      // because no ERO, ORB epoch is empty
-      await checkEpochNumber();
-      await checkState(State.AcceptingNRB);
-    });
-
-    after('check ORB epoch#4 parameters', async () => {
-      const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
-        await rootchain.getEpoch(currentFork, currentEpoch - 1);
-
-      startBlockNumber.should.be.bignumber.equal(currentBlockNumber);
-      endBlockNumber.should.be.bignumber.equal(endBlockNumber);
-      isEmpty.should.be.equal(true);
-    });
-  });
-
-  describe('Epoch#5', async () => {
-    before('check parameters', async () => {
-      await checkState(State.AcceptingNRB);
-      await checkBlockNumber();
-
-      const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
-        await rootchain.getEpoch(currentFork, currentEpoch);
-
-      startBlockNumber.should.be.bignumber.equal(currentBlockNumber + 1);
-      endBlockNumber.should.be.bignumber.equal(startBlockNumber.add(NRBEpochLength).sub(1));
-    });
-
-    it('next empty ORB epoch#4 should be prepared', async () => {
-      // submits `NRBEpochLength` NRBs
-      await Promise.all(range(NRBEpochLength).map(submitDummyNRB));
-
-      currentEpoch += 2;
-
-      // because no ERO, ORB epoch is empty
-      await checkEpochNumber();
-      await checkState(State.AcceptingNRB);
-    });
-
-    after('check ORB epoch#4 parameters', async () => {
-      const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
-        await rootchain.getEpoch(currentFork, currentEpoch - 1);
-
-      startBlockNumber.should.be.bignumber.equal(currentBlockNumber);
-      endBlockNumber.should.be.bignumber.equal(endBlockNumber);
-      isEmpty.should.be.equal(true);
-    });
-  });
-
-  describe('Epoch#7', async () => {
-    it('user can make a enter request', async () => {
-      (await rootchain.getNumEROs()).should.be.bignumber.equal(0);
+    it(`NRBEpoch#${NRBEPochNumber}: user can make a enter request`, async () => {
+      (await rootchain.getNumEROs()).should.be.bignumber.equal(numRequests);
 
       await Promise.all(others.map(other =>
         rootchain.startEnter(other, emptyBytes32, emptyBytes32, { from: other, value: etherAmount })
       ));
 
-      (await rootchain.getNumEROs()).should.be.bignumber.equal(others.length);
+      numRequests += others.length;
+
+      (await rootchain.getNumEROs()).should.be.bignumber.equal(numRequests);
     });
 
-    it('operator submits NRBs', async () => {
+    it(`NRBEpoch#${NRBEPochNumber}: operator submits NRBs`, async () => {
       await Promise.all(range(NRBEpochLength).map(submitDummyNRB));
       currentEpoch += 1;
       await checkEpochNumber();
       await checkState(State.AcceptingORB);
     });
 
-    it('operator submits ORBs', async () => {
+    it(`ORBEpoch#${ORBEPochNumber}: operator submits ORBs`, async () => {
       const [requestStart, requestEnd, startBlockNumber, endBlockNumber, forkedBlockNumber, isEmpty, initialized, isRequest, userActivated, finalized] =
         await rootchain.getEpoch(currentFork, currentEpoch);
 
@@ -221,5 +163,45 @@ contract('RootChain', async ([
       await checkEpochNumber();
       await checkState(State.AcceptingNRB);
     });
+  };
+
+  describe('Epoch#1', async () => {
+    const epochNumber = 1;
+    testEpochsWithoutRequest(epochNumber);
+  });
+
+  describe('Epoch#3', async () => {
+    const epochNumber = 3;
+    testEpochsWithoutRequest(epochNumber);
+  });
+
+  describe('Epoch#5', async () => {
+    const epochNumber = 5;
+    testEpochsWithoutRequest(epochNumber);
+  });
+
+  describe('Epoch#7', async () => {
+    const epochNumber = 7;
+    testEpochsWithRequest(epochNumber);
+  });
+
+  describe('Epoch#9', async () => {
+    const epochNumber = 9;
+    testEpochsWithRequest(epochNumber);
+  });
+
+  describe('Epoch#11', async () => {
+    const epochNumber = 11;
+    testEpochsWithRequest(epochNumber);
+  });
+
+  describe('Epoch#13', async () => {
+    const epochNumber = 13;
+    testEpochsWithoutRequest(epochNumber);
+  });
+
+  describe('Epoch#15', async () => {
+    const epochNumber = 15;
+    testEpochsWithRequest(epochNumber);
   });
 });
