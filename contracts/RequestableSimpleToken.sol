2fa989b603dcaa52fa96a6b13484fd7fde5fbba0
4000D <dkdkajej@gmail.com>
2018-10-23 13:41:14 +0900

    contracts: remove openzeppelin-solidity

--- contracts/RequestableSimpleToken.sol
+++ contracts/RequestableSimpleToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./lib/SafeMath.sol";
+import "./lib/Ownable.sol";
 
 contract RequestableSimpleToken is Ownable {
   using SafeMath for *;
