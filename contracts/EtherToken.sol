646b53efdf7e061c8512af9ba61b7eb3e3fe17f4
4000D <dkdkajej@gmail.com>
2019-02-14 22:03:28 +0900

    contracts: EtherToken added with erc20-wrapper submodule

---
+++ contracts/EtherToken.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.24;
+
+import "../requestable-erc20-wrapper-token/contracts/RequestableERC20Wrapper.sol";
+
+
+/**
+ * @title     EtherToken
+ * @notice    EtherToken is a requestable token that can exchange ETH at a 1:1 ratio.
+ *            This contract is deployed both in root chain and in child chain. But in root chain,
+ *            It doesn't support ETH swap, it just takes another ERC20 as a exchangable token.
+ *            However, EtherToken in child chain takes (P)ETH to
+ */
+contract EtherToken is RequestableERC20Wrapper {
+  bool public swapEnabled;
+
+  // EtherToken in root chain disables ether swap, but EtherToken in child chain allows it.
+  constructor(
+    bool _development,
+    ERC20 _token,
+    bool _swapEnabled
+  ) public RequestableERC20Wrapper(_development, _token) {
+    bool noToken = address(_token) == address(0);
+
+    // in production, Exchangable asset must be either ERC20 or ETH, not both.
+    require(_development || (noToken && !_swapEnabled || !noToken && _swapEnabled));
+    swapEnabled = _swapEnabled;
+  }
+
+  function() public payable {
+    swapFromEth();
+  }
+
+  // swap ETH to token
+  function swapFromEth() public payable {
+    require(swapEnabled);
+
+    mint(msg.sender, msg.value);
+  }
+
+  // swap token to ETH
+  function swapToEth(uint _amount) public {
+    require(swapEnabled);
+
+    require(transferFrom(msg.sender, this, _amount));
+    burn(msg.sender, _amount);
+  }
+}
---
+++ contracts/misc/_import.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
