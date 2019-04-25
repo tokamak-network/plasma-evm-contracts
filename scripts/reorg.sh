865426e0e0912d6dd9000a0b955487c8dad31e50
Geonwoo, Shin <dn2757@gmail.com>
2019-04-25 15:24:07 +0900

    scripts: set gasprice depending on txremoved flag

--- scripts/reorg.sh
+++ scripts/reorg.sh
@@ -2,6 +2,7 @@
 
 FORKINTERVAL=${1:-10}
 REORGINTERVAL=${2:-5}
+TXREMOVED=${3:-false}
 
 ENODE="enode://bb9c918c07cfb577ecdfd3785a8da4c7fecc2ae1838191e7b43b35214d480e9d5eae2775bafeea87038243010edd7670a686ae30c5d5a025b03582fdedec58ba@[::]:30304?discport=0" # --nodekeyhex 5e048defbb082a83db7aa0c508696b70ecfb25de3973d7ea9cd5eeea2d295c17
 PRIVATEKEY1="b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291"
@@ -13,11 +14,12 @@ echo "forkinterval=$FORKINTERVAL, reorginterval=$REORGINTERVAL"
 
 geth attach http://127.0.0.1:8545 << EOF | grep "Data: " | sed "s/Data: //"
 
-personal.importRawKey('$PRIVATEKEY1', ""); personal.importRawKey('$PRIVATEKEY2', "");
-personal.unlockAccount('$ADDRESS1', "", 0); personal.unlockAccount('$ADDRESS2', "", 0);
-console.log('Data: accounts=[' + eth.accounts + ']');
-
-EOF
+if [ "$TXREMOVED" = true ]
+then
+    geth --exec "miner.setGasPrice(1e18)" attach http://127.0.0.1:8645 | grep "Data: " | sed "s/Data: //"
+else
+    geth --exec "miner.setGasPrice(1)" attach http://127.0.0.1:8645 | grep "Data: " | sed "s/Data: //"
+fi
 
 # reference: https://ethereum.stackexchange.com/questions/10033/can-i-pass-arguments-to-a-js-script-on-geth 
 echo "miner.stop(); admin.addPeer('$ENODE'); admin.sleepBlocks($REORGINTERVAL); console.log('Data: mining=' + eth.mining + ', peers=' + admin.peers.length + ', number=' + eth.blockNumber);" > $(pwd)/scripts/fork.js
