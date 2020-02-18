85a4c10a925dc132753b438a0fc4069701e12d0b
4000D <dkdkajej@gmail.com>
2020-02-18 14:23:18 +0900

    migrations: add manager deploy script

---
+++ migrations/5_deploy_managers.js
@@ -0,0 +1,52 @@
+const { createCurrency } = require('@makerdao/currency');
+
+const _WTON = createCurrency('WTON');
+
+const RootChain = artifacts.require('RootChain.sol');
+const TON = artifacts.require('TON.sol');
+const WTON = artifacts.require('WTON.sol');
+const RootChainRegistry = artifacts.require('RootChainRegistry.sol');
+const DepositManager = artifacts.require('DepositManager.sol');
+const SeigManager = artifacts.require('SeigManager.sol');
+
+// 1024 blocks
+const WITHDRAWAL_DELAY = process.env.WITHDRAWAL_DELAY || '0x400';
+
+// 100 WTON per block as seigniorage
+const SEIG_PER_BLOCK = process.env.SEIG_PER_BLOCK || '100.0';
+
+module.exports = async function (deployer) {
+  const rootchain = await RootChain.deployed();
+  const ton = await TON.deployed();
+
+  const wton = await deployer.deploy(WTON, ton.address);
+  const registry = await deployer.deploy(RootChainRegistry);
+  const depositManager = await deployer.deploy(
+    DepositManager,
+    wton.address,
+    registry.address,
+    WITHDRAWAL_DELAY,
+  );
+  const seigManager = await deployer.deploy(
+    SeigManager,
+    ton.address,
+    wton.address,
+    registry.address,
+    depositManager.address,
+    _WTON(SEIG_PER_BLOCK).toFixed('ray'),
+  );
+
+  // add WSTON minter role to seig manager
+  await wton.addMinter(seigManager.address);
+
+  // set seig manager to contracts
+  await Promise.all([
+    depositManager,
+    wton,
+  ].map(contract => contract.setSeigManager(seigManager.address)));
+
+  await rootchain.setSeigManager(seigManager.address);
+
+  // register root chain and deploy coinage
+  await registry.registerAndDeployCoinage(rootchain.address, seigManager.address);
+};
