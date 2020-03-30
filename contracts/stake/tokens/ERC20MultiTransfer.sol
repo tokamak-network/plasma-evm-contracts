9c6d3719eaac4278b6e23f4cd4c923c29cd78ab9
4000D <dkdkajej@gmail.com>
2020-03-30 17:14:32 +0900

    contracts/stake: add MTON

---
+++ contracts/stake/tokens/ERC20MultiTransfer.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.12;
+
+import { Ownable } from "../../../node_modules/openzeppelin-solidity/contracts/ownership/OWnable.sol";
+import { IERC20 } from "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+
+
+contract ERC20MultiTransfer {
+  using SafeERC20 for IERC20;
+
+  function transferFixedAmount(
+    IERC20 token,
+    address[] calldata targets,
+    uint256 amount
+  ) external {
+    token.safeTransferFrom(msg.sender, address(this), amount * targets.length);
+
+    for (uint256 i = 0; i < targets.length; i++) {
+      address target = targets[i];
+      token.safeTransfer(target, amount);
+    }
+  }
+}
---
+++ contracts/stake/tokens/MTON.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.12;
+
+import { Ownable } from "../../../node_modules/openzeppelin-solidity/contracts/ownership/OWnable.sol";
+import { ERC20Mintable } from "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import { ERC20Detailed } from "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import { ERC165Checker } from "../../../node_modules/openzeppelin-solidity/contracts/introspection/ERC165Checker.sol";
+
+import { SeigManagerI } from "../interfaces/SeigManagerI.sol";
+import { SeigToken } from "./SeigToken.sol";
+
+
+/**
+ * @dev Current implementations is just for testing seigniorage manager.
+ */
+contract MTON is Ownable, ERC20Mintable, ERC20Detailed, SeigToken {
+  constructor() public ERC20Detailed("Marketing Tokamak Network Token", "MTON", 18) {}
+
+  function setSeigManager(SeigManagerI _seigManager) external {
+    revert("MTON: MTON doesn't allow setSeigManager");
+  }
+}
