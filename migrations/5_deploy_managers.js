11e95fb0dfd1d0fa9f1b62ad0f933fa29d9ba85f
modagi <modagi@gmail.com>
2020-09-06 15:16:34 +0900

    migration: added CoinageFactory deployment

--- migrations/5_deploy_managers.js
+++ migrations/5_deploy_managers.js
@@ -8,6 +8,7 @@ const WTON = artifacts.require('WTON.sol');
 const RootChainRegistry = artifacts.require('RootChainRegistry.sol');
 const DepositManager = artifacts.require('DepositManager.sol');
 const SeigManager = artifacts.require('SeigManager.sol');
+const CoinageFactory = artifacts.require('CoinageFactory.sol');
 
 // 1024 blocks
 const WITHDRAWAL_DELAY = process.env.WITHDRAWAL_DELAY || '0x400';
@@ -38,6 +39,10 @@ module.exports = async function (deployer, network) {
     depositManager.address,
     _WTON(SEIG_PER_BLOCK).toFixed('ray'),
   );
+
+  const factory = await deployer.deploy(CoinageFactory);
+  await seigManager.setCoinageFactory(factory.address);
+  await factory.setSeigManager(seigManager.address);
 
   // add WSTON minter role to seig manager
   await wton.addMinter(seigManager.address);
