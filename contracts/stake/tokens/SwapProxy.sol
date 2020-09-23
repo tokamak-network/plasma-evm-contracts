fff1b4ec8c93d2c07b05675afa9ae3c8254d6089
modagi <modagi@gmail.com>
2020-09-23 11:29:04 +0900

    add SwapProxy contract

---
+++ contracts/stake/tokens/SwapProxy.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.12;
+
+import { ERC165 } from "../../../node_modules/openzeppelin-solidity/contracts/introspection/ERC165.sol";
+
+contract SwapProxy is ERC165 {
+  constructor() public {
+    _registerInterface(SwapProxy(this).onApprove.selector);
+  }
+
+  function onApprove(
+    address owner,
+    address spender,
+    uint256 tonAmount,
+    bytes calldata data
+  ) external returns (bool) {
+    return true;
+  }
+}
