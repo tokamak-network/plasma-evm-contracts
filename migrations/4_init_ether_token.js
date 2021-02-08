957e18b6cbc56d2f913e2f56b84fa2dbc3bcd24e
SEFI2 <kadirpili@gmail.com>
2021-02-08 17:12:06 +0300

    Change migration file 4

---
+++ migrations/4_init_ether_token.js
@@ -0,0 +1,18 @@
+const Layer2 = artifacts.require('Layer2.sol');
+const EtherToken = artifacts.require('EtherToken.sol');
+
+module.exports = async function (deployer, network) {
+  // skip production network
+  if (
+    network.includes('faraday') ||
+    network.includes('mainnet') ||
+    network.includes('rinkeby') ||
+    network.includes('development') ||
+    network.includes('ganache')
+  ) return;
+
+  const etherToken = await EtherToken.deployed();
+  const layer2 = await Layer2.deployed();
+
+  await etherToken.init(layer2.address);
+};
