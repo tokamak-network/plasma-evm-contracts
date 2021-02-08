9cceb311ee97473d2f407de0cf67720e547d76b4
SEFI2 <kadirpili@gmail.com>
2021-02-08 17:09:50 +0300

    Change migration file 2

--- migrations/2_deploy_ether_token.js
+++ migrations/2_deploy_ether_token.js
@@ -7,16 +7,20 @@ const baseTokenAddress = process.env.BASE_TOKEN || '0x00000000000000000000000000
 
 const swapEnabled = true;
 
-module.exports = function (deployer, network) {
+module.exports = async function (deployer, network) {
   // skip production network
-  if (network.includes('faraday') || network.includes('mainnet') || network.includes('rinkeby') || network.includes('development')) return;
+  if (
+    network.includes('faraday') ||
+    network.includes('mainnet') ||
+    network.includes('rinkeby') ||
+    network.includes('development')
+  ) return;
 
   if (development || baseTokenAddress === '0x0000000000000000000000000000000000000000') {
-    deployer.deploy(TON)
-      .then(token => deployer.deploy(EtherToken, development, token.address, swapEnabled))
-      .catch(e => { throw e; });
+    await deployer.deploy(TON);
+    const token = await TON.deployed();
+    await deployer.deploy(EtherToken, development, token.address, swapEnabled);
   } else {
-    deployer.deploy(EtherToken, development, baseTokenAddress, swapEnabled)
-      .catch(e => { throw e; });
+    await deployer.deploy(EtherToken, development, baseTokenAddress, swapEnabled);
   }
 };
