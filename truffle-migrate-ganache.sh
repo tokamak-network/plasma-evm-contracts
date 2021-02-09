9315a3274cc27492d69b2c1a6e7ba5b774201fe7
SEFI2 <kadirpili@gmail.com>
2021-02-09 12:06:23 +0300

    Add script to run migrations in order

---
+++ truffle-migrate-ganache.sh
@@ -0,0 +1,5 @@
+truffle migrate --network ganache -f 7 --to 7
+truffle migrate --network ganache -f 5 --to 5
+truffle migrate --network ganache -f 2 --to 2
+truffle migrate --network ganache -f 4 --to 4
+truffle migrate --network ganache -f 3 --to 3
