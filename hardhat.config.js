616749242ff2266bea61974a76e86e5648622c82
SEFI2 <kadirpili@gmail.com>
2021-06-26 14:11:25 +0300

    Add optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,11 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: {
     version: '0.5.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
   },
-
 };
